908fb55ab562bc7d16e83c52e8be5e4ce1dbb5c1
Luara Amaral <98507687+Luaraamaral@users.noreply.github.com>
2023-05-22 10:06:25 -0300

    Initial Commit

---
+++ src/main/java/br/com/luara/files3/Files3Application.java
@@ -0,0 +1,13 @@
+package br.com.luara.files3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Files3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Files3Application.class, args);
+	}
+
+}
